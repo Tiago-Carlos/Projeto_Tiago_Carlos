4fcb0b6ffcef10ad7d9c6040e684e01558bf90b4
Tiago Carlos <tiagozero3@gmail.com>
2021-08-29 11:15:48 -0300

    commit inicial

---
+++ Class_A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class A
+{
+private:
+    int A1;
+    float A2;
+public:
+    A();
+    ~A();
+    
+    void set_A1(int A) { this->A1 = A; };
+    void set_A2(float A) { this->A2 = A; };
+
+    int get_A1() { return this->A1; };
+    float get_A2() { return this->A2; }
+
+    void MA1() { std::cout << "MA1\n"; };
+    void MA2() { std::cout << "MA2\n"; };
+};
+
+int main() {
+    return 0;
+}
---
+++ Class_B.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class B
+{
+private:
+    int B1;
+    float B2;
+public:
+    B();
+    ~B();
+    
+    void set_B1(int B) { this->B1 = B; };
+    void set_B2(float B) { this->B2 = B; };
+
+    int get_B1() { return this->B1; };
+    float get_B2() { return this->B2; }
+
+    void MB1() { std::cout << "MB1\n"; };
+    void MB2() { std::cout << "MB2\n"; };
+};
+
+int main() {
+    return 0;
+}
