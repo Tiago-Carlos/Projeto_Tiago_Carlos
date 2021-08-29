372a1b4eb35698876adbbb69573ae77813f8f18f
Tiago Carlos <tiagozero3@gmail.com>
2021-08-29 14:21:38 -0300

    Criada a classe D

---
+++ Class_D.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class D
+{
+private:
+    int D1;
+    int D2;
+public:
+    D();
+    ~D();
+    
+    void set_D1(int D) { this->D1 = D; };
+    void set_D2(int D) { this->D2 = D; };
+
+    int get_D1() { return this->D1; };
+    int get_D2() { return this->D2; }
+
+    void MD1() { std::cout << "MD1\n"; };
+    void MD2() { std::cout << "MD2\n"; };
+};
+
+int main() {
+    return 0;
+}
